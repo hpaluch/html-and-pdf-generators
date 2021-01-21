448b9db7adc54dbf16049ea20285e5e155aa31ca
Henryk Paluch <henryk.paluch@pickering.cz>
2021-01-21 07:41:33 +0100

    Prototype scripts to build HTML & PDF Under Windows

---
+++ sphinx/build_sphinx_html.cmd
@@ -0,0 +1,20 @@
+@echo off
+@setlocal
+:: Script to build HTML documentation using Sphinx
+:: You need to have installed:
+:: - Python 3
+:: - Sphinx via PIP
+::
+:: NOTE: Please review and update path for Sphinx 
+::       as needed
+
+
+set SPHINXBUILD=c:\Program Files\Python37\Scripts\sphinx-build.exe
+
+"%SPHINXBUILD%" -M html source build || goto error
+exit /b 0
+
+:error
+echo "Error %ERROLEVEL% while running Sphinx"
+exit /b 1
+
---
+++ sphinx/build_sphinx_latexpdf.cmd
@@ -0,0 +1,22 @@
+@echo off
+@setlocal
+:: Script to build PDF documentation using Sphinx and TeX Live
+:: You need to have installed:
+:: - Python 3
+:: - Sphinx via PIP
+:: - TeX Live from https://mirrors.nic.cz/tex-archive/systems/texlive/Images/
+::
+:: NOTE: Please review and update path for both Sphinx and/or TeX Live
+::       as needed
+
+set SPHINXBUILD=c:\Program Files\Python37\Scripts\sphinx-build.exe
+PATH=e:\texlive\2020\bin\win32;%PATH%
+
+"%SPHINXBUILD%" -M latexpdf source build || goto error
+exit /b 0
+
+:error
+echo "Error %ERROLEVEL% while running Sphinx"
+exit /b 1
+
+
